2f8fd82e02b4cf433f9b39911b1fdcdbf6237853
Newxra <161119435+Newxra@users.noreply.github.com>
2024-11-27 20:25:51 +0800

    google 11/27

--- app/src/main/java/com/example/myapplication/MainPlay.java
+++ app/src/main/java/com/example/myapplication/MainPlay.java
@@ -3,12 +3,33 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainPlay extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.OnStreetViewPanoramaReadyCallback;
+import com.google.android.gms.maps.StreetViewPanorama;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.SupportStreetViewPanoramaFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.StreetViewPanoramaCamera;
+import com.google.android.gms.maps.model.StreetViewPanoramaOrientation;
+
+public class MainPlay extends AppCompatActivity implements OnMapReadyCallback, OnStreetViewPanoramaReadyCallback {
+
+    private LatLng mapCoordinate;
+
+    private GoogleMap mapIns;
+    private StreetViewPanorama streetViewIns;
+
+    private SupportMapFragment mapFragment;
+    private SupportStreetViewPanoramaFragment streetViewFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +41,46 @@ public class MainPlay extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // 台灣正中心的經緯度
+        mapCoordinate = new LatLng(23.975667, 120.973861);
+
+        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_fragment);
+        if(mapFragment != null)
+            mapFragment.getMapAsync((OnMapReadyCallback) this);
+
+        streetViewFragment = (SupportStreetViewPanoramaFragment) getSupportFragmentManager().findFragmentById(R.id.streetview_fragment);
+        if(streetViewFragment != null)
+            streetViewFragment.getStreetViewPanoramaAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap map) {
+        mapIns = map;
+
+        // 在地圖上新增Marker
+        mapIns.addMarker(new MarkerOptions().position(mapCoordinate).title("Location"));
+        mapIns.moveCamera(CameraUpdateFactory.newLatLngZoom(mapCoordinate, 8f));
+    }
+
+    @Override
+    public void onStreetViewPanoramaReady(StreetViewPanorama streetViewPanorama) {
+        streetViewIns = streetViewPanorama;
+
+        streetViewPanorama.setPanningGesturesEnabled(true);
+        streetViewPanorama.setUserNavigationEnabled(true);
+        streetViewPanorama.setZoomGesturesEnabled(true);
+        streetViewPanorama.animateTo(
+                new StreetViewPanoramaCamera.Builder().orientation(new StreetViewPanoramaOrientation(20, 20))
+                        .zoom(streetViewPanorama.getPanoramaCamera().zoom)
+                        .build(), 2000
+        );
+
+        LatLng FUJEN = new LatLng(25.0326369,121.4342057);
+        setStreetViewPosition(FUJEN);
+    }
+
+    public void setStreetViewPosition(LatLng latLng) {
+        streetViewIns.setPosition(latLng);
     }
 }
