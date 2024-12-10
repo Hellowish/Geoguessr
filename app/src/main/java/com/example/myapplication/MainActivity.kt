a7eb249dc1c4d59421f75848e0107807841abae6
kehua <kehuanwu@gmail.com>
2024-12-10 23:11:42 +0800

    slight animation

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.myapplication
 
 import android.content.Intent
-import android.media.MediaPlayer
+import android.graphics.drawable.AnimationDrawable
 import android.os.Bundle
+import android.view.animation.AnimationUtils
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.media.MediaPlayer
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,36 +22,49 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // Initialize MediaPlayer with a music file
-        mediaPlayer = MediaPlayer.create(this, R.raw.background_music) // Place music in res/raw folder
+        mediaPlayer = MediaPlayer.create(this, R.raw.background_music)
+        mediaPlayer.isLooping = true
+        mediaPlayer.start()
 
-        mediaPlayer.isLooping = true // Loop the music
-        mediaPlayer.start() // Start playing music
+        // Start background animation
+        val backgroundLayout = findViewById<ConstraintLayout>(R.id.main)
+        val background = backgroundLayout.background
+        if (background is AnimationDrawable) {
+            background.setEnterFadeDuration(500)
+            background.setExitFadeDuration(500)
+            background.start()
+        }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        // Start button animation
+        val button = findViewById<ImageButton>(R.id.button)
+        val animation = AnimationUtils.loadAnimation(this, R.anim.button_animation)
+        button.startAnimation(animation)
+
+        // Button click listener to navigate to Home activity
+        button.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(backgroundLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Set up button click listener to open MainActivity2
-        findViewById<ImageButton>(R.id.button).setOnClickListener {
-            val intent = Intent(this, Home::class.java)
-            startActivity(intent)
         }
     }
 
     override fun onPause() {
         super.onPause()
-        mediaPlayer.pause() // Pause music when activity goes to background
+        mediaPlayer.pause()
     }
 
     override fun onResume() {
         super.onResume()
-        mediaPlayer.start() // Resume music when activity comes to foreground
+        mediaPlayer.start()
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        mediaPlayer.release() // Release resources when activity is destroyed
+        mediaPlayer.release()
     }
 }
